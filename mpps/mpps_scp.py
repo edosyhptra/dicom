d13d3623727eabebd7ad44298774def5d7d10778
edosyhptra <edo23syahputra@gmail.com>
2024-06-22 15:13:46 +0700

    mpps set

---
+++ mpps/mpps_scp.py
@@ -0,0 +1,19 @@
+from pydicom.dataset import Dataset
+
+from pynetdicom import AE, evt
+from pynetdicom.sop_class import ModalityPerformedProcedureStep
+import mpps_handlers
+
+
+handlers = [(evt.EVT_N_CREATE, mpps_handlers.handle_create), (evt.EVT_N_SET, mpps_handlers.handle_set)]
+
+# Initialise the Application Entity and specify the listen port
+ae = AE(
+    ae_title='admin-scp'
+)
+
+# Add the supported presentation context
+ae.add_supported_context(ModalityPerformedProcedureStep)
+
+# Start listening for incoming association requests
+ae.start_server(("127.0.0.1", 1234), evt_handlers=handlers)
